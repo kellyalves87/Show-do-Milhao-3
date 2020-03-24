b6b0fcc7221df7493620a0930962638e217d1bd1
kelly <kellyp.alves87@gmail.com>
2020-03-24 20:03:21 -0300

    first commit

---
+++ quiz-geography.js
@@ -0,0 +1,13 @@
+
+
+if (questionOne == 1){
+  document.getElementById('correctAnswerA').innerHTML = "América do Sul";
+}
+
+if (questionTwo == 2) {
+  document.getElementById('correctAnswerB').innerHTML = "Sudeste";
+}
+
+if (questionThree == 3) {
+  document.getElementById('correctAnswerC').innerHTML = "Brasília";
+}
---
+++ quiz-history.js
@@ -0,0 +1,13 @@
+
+
+if (questionHistoryOne == 1){
+  document.getElementById('correctAnswerA').innerHTML = "São Vicente";
+}
+
+if (questionHistoryTwo == 1) {
+  document.getElementById('correctAnswerA').innerHTML = "Dom Pedro I";
+}
+
+if (questionHistoryThree == 3) {
+  document.getElementById('correctAnswerC').innerHTML = "Marechal Deodoro";
+}
